6dd71168498f9e30d9a09ac2950d0ac2c26cf9b1
Ariel Monterroso <bmonterrosoalvarado@gmail.com>
2021-04-18 23:27:38 -0600

    commit inicial

---
+++ Api/src/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+ app.get('/iecho', (req, res) => {
+    const param = req.query.text;
+    if (param) 
+    {
+        const text = param.toLocaleLowerCase();
+        const reverseText = text.split('').reverse().join('');
+        
+        text === reverseText ? 
+        res.status(200).send({"text": reverseText,"palindrome": true}) : 
+        res.status(200).send({"text": reverseText,"palindrome": false});
+
+    } else
+    {
+        res.status(400).send({
+            "error": "no text"
+        })
+    }
+ })
+
+
+
+
+
+app.listen(PORT,()=> {
+    console.log(`server on port: ${PORT}`);
+})
